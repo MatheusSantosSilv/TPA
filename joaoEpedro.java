810e578ee4c12eb9cd1220225849ee65f55ae8ed
Theuz <112184110+MatheusSantosSilv@users.noreply.github.com>
2022-11-18 09:19:09 -0300

    Add files via upload

---
+++ joaoEpedro.java
@@ -0,0 +1,19 @@
+package teste;
+import java.util.Scanner;
+public class teste2 {
+
+	public static void main(String[] args) {
+		
+		double joao = 1.34, pedro = 1.45, i = 1;
+		while (joao<=pedro) {
+				joao = (joao+ 0.025);
+				pedro = (pedro+0.020);	
+		i++;	
+	}	
+		System.out.println("será " +i+ " anos para o joao ser maior que pedro");
+					
+		}
+		
+		
+	}
+
