f392ebe3497d8075cfc8ad6088300c10f07cd239
Theuz <112184110+MatheusSantosSilv@users.noreply.github.com>
2022-12-02 09:24:24 -0300

    Add files via upload

---
+++ doWhileFor/joaoPedro.java
@@ -0,0 +1,10 @@
+package doWhile;
+
+public class joaoPedro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ doWhileFor/tabuada.java
@@ -0,0 +1,19 @@
+package doWhile;
+import java.util.*;
+public class tabuada {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		int i=1, a,b=1;
+		System.out.println("Insira o número desejado");
+		a = ler.nextInt();
+	
+		while(i<=10) {
+			b =(a*i);
+			System.out.println(a+"x"+i+"="+b);
+			i++;
+		}
+
+	}
+
+}
